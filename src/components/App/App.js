0cddfd901fc4a224f90647d7189cde3ebf6c6d2e
Jake Crane <jake@jakecrane.com>
2017-05-07 14:59:41 -0500

    Added Login functionality with firebase

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,19 +2,47 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Login from "../Login/Login";
+import MainPage from "../MainPage/MainPage";
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import firebase from "firebase";
 
-class App extends Component {
+export default class App extends Component {
+  static loggedIn = false;
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Jake's React Project</h2>
+          <div>
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Jake's React Project</h2>
+          </div>
         </div>
-        <Login />
+        <div>
+          <Router>
+            <div>
+              <Route exact path="/" component={MainPage}></Route>
+              <Route path="/login" component={Login}></Route>
+            </div>
+          </Router>
+        </div>
       </div>
     );
   }
 }
 
-export default App;
+var config = {
+  apiKey: "AIzaSyC96vVF9PLyYX3Tr0Ilz7XuKBepQx982AY",
+  authDomain: "jakestest-cd808.firebaseapp.com",
+  databaseURL: "https://jakestest-cd808.firebaseio.com",
+  projectId: "jakestest-cd808",
+  storageBucket: "jakestest-cd808.appspot.com",
+  messagingSenderId: "819066694875"
+};
+firebase.initializeApp(config);
+firebase.auth().onAuthStateChanged(function (user) {
+  if (user) {
+    App.loggedIn = true;
+  } else {
+
+  }
+});
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,13 +1,27 @@
 import React from "react";
 import './Login.css';
+import { Redirect } from 'react-router-dom'
+import firebase from "firebase";
+import App from "../App/App";
 
 export default class Login extends React.Component {
+    constructor() {
+        super();
+        this.state = { loggedIn: false };
+        var self = this;
+        firebase.auth().onAuthStateChanged(function (user) {
+            if (user) {
+                self.setState({ loggedIn: true });
+            } else {
+
+            }
+        });
+    }
     login() {
-        alert('Username:'
-            + this.state.username
-            + '\n'
-            + 'Password:'
-            + this.state.password);
+        firebase.auth().signInWithEmailAndPassword(this.state.username, this.state.password)
+            .catch(function (error) {
+                console.error(error);
+            });
     }
     handleUsernameChange(e) {
         this.setState({ username: e.target.value });
@@ -15,24 +29,35 @@ export default class Login extends React.Component {
     handlePasswordChange(e) {
         this.setState({ password: e.target.value });
     }
+    handleKeypress(e) {
+        if (e.charCode === 13)
+            this.login();
+    }
     render() {
-        return (
-            <div className="outerFormContainer">
-                <div className="formContainer">
-                    <form>
-                        <div className="loginTitle">Login</div>
+        if (App.loggedIn || this.state.loggedIn) {
+            return (<Redirect to={{
+                pathname: '/',
+                state: { from: this.props.location }
+            }} />);
+        } else {
+            return (
+                <div className="outerFormContainer">
+                    <div className="formContainer">
                         <div>
-                            <input id="username" placeholder="Username" onChange={this.handleUsernameChange.bind(this)} required />
+                            <div className="loginTitle">Login</div>
+                            <div>
+                                <input id="username" placeholder="Username" onChange={this.handleUsernameChange.bind(this)} onKeyPress={this.handleKeypress.bind(this)} required />
+                            </div>
+                            <div>
+                                <input id="password" placeholder="Password" type="password" onChange={this.handlePasswordChange.bind(this)} onKeyPress={this.handleKeypress.bind(this)} required />
+                            </div>
+                            <div>
+                                <button onClick={this.login.bind(this)}>Login</button>
+                            </div>
                         </div>
-                        <div>
-                            <input id="password" placeholder="Password" type="password" onChange={this.handlePasswordChange.bind(this)} required />
-                        </div>
-                        <div>
-                            <button onClick={this.login.bind(this)}>Login</button>
-                        </div>
-                    </form>
+                    </div>
                 </div>
-            </div>
-        );
+            );
+        }
     }
 }
